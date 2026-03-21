ece1949244a42f940db4e1911a968814af71de3d
AliKhadr <49314744+AliKhadr@users.noreply.github.com>
2026-03-21 18:27:51 +0200

    Added Category table to database and created initial controller and view

--- MvcProjectAspNet/Data/ApplicationDbContext.cs
+++ MvcProjectAspNet/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MvcProjectAspNet.Models;
 
 namespace MvcProjectAspNet.Data
 {
@@ -8,5 +9,8 @@ namespace MvcProjectAspNet.Data
         {
 
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
+    
