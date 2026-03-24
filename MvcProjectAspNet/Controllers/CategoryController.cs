7dde717067646b407e5cfc1c73bb8cad72903584
AliKhadr <49314744+AliKhadr@users.noreply.github.com>
2026-03-24 23:34:57 +0200

    Displaying Model items in view

--- MvcProjectAspNet/Controllers/CategoryController.cs
+++ MvcProjectAspNet/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcProjectAspNet.Data;
+using MvcProjectAspNet.Models;
 
 namespace MvcProjectAspNet.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
