b43bdd23c64ad9be41c1531a1584b012bd6354be
AliKhadr <49314744+AliKhadr@users.noreply.github.com>
2026-04-13 19:36:38 +0200

    Setting up Razor Pages. Packages, DBContext, Database, etc

---
+++ RazorProjectAspNet/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorProjectAspNet.Models;
+
+namespace RazorProjectAspNet.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Seed data for Categories
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Horror", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Romance", DisplayOrder = 3 }
+            );
+        }
+    }
+}
